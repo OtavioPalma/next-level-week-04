36f9f282ef760e9bda09bdb17b909a8418bd4289
Otavio Palma <otaviopalma@gmail.com>
2021-02-23 11:29:50 -0300

    01 - Initial Next.js Setup

---
+++ src/components/ExperienceBar.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import classes from '../styles/components/ExperienceBar.module.scss';
+
+export const ExperienceBar = () => {
+  return (
+    <header className={classes.experienceBar}>
+      <span>0 xp</span>
+
+      <div>
+        <div style={{ width: '50%' }}></div>
+
+        <span
+          className={classes.experienceBar_currentExperience}
+          style={{ left: '50%' }}
+        >
+          300px
+        </span>
+      </div>
+
+      <span>600 xp</span>
+    </header>
+  );
+};
